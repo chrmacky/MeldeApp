cc9c324dfc721f9547fec46391186e116715ec00
Christoph Lemke <christophlemke@nielss-imac.local>
2018-10-09 12:39:09 +0200

    Initial Commit

---
+++ MeldeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeldeApp
+//
+//  Created by Christoph Lemke on 09.10.18.
+//  Copyright © 2018 Christoph Lemke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
